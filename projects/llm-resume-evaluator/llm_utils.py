cfadf1409ee6b14e599f04e6f4fc86e492b0fbd4
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2026-08-03 23:54:55 +0530

    feat: create resuable llm-utility module for query parsing

---
+++ projects/llm-resume-evaluator/llm_utils.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+
+load_dotenv()
+
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("GROQ_API_KEY environment variable is not set.")
+
+client = Groq(api_key=my_api_key)
+DEFAULT_MODEL = "llama-3.3-70b-versatile"
+
+def parse_query(system_prompt:str, query_prompt: str, response_format={"type": "json_object"}):
+
+   messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": query_prompt}
+    ]
+
+   response = client.chat.completions.create(
+        model = DEFAULT_MODEL,
+         messages = messages,
+         response_format = response_format
+   )
+
+   return response
